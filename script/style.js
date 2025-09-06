a0c2e0d01709393a49c139a3e8488975440aad61
LOPPIN Nicolas <loppin.n66@gmail.com>
2025-09-06 18:16:07 +0200

    PUSH : Base Style

---
+++ script/style.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        cream: "#F5F3F0",
+        "soft-blue": "#A8C8E1",
+        "light-gray": "#E8E8E8",
+        "warm-beige": "#E6D7C3",
+        "deep-blue": "#7BA7D1",
+      },
+      fontFamily: {
+        sans: ["Inter", "system-ui", "sans-serif"],
+      },
+    },
+  },
+};
